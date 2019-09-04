2895baf57ca841ee3465363185cf4dd880284171
Ryota <rytswd@gmail.com>
2019-09-05 00:52:18 +0100

    Clean up further

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,3 +1,5 @@
-describe('TODO - Add a test suite', () => {
-  it('TODO - Add a test', async () => {});
+describe("comvent handles message", () => {
+  it("should return found when a match is found", async () => {});
+  it("should return empty string when there is no match", async () => {});
+  it("should fail if no comment provided", async () => {});
 });
